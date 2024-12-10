f76f26e5b25391310f8b8294674f946b4ffa3d3d
rnrran <randykamalhusen@gmail.com>
2024-12-10 17:07:23 +0800

    feat(profile): add changeProfile page

--- app/src/main/java/com/sem5/codemy/features/profile/presentation/components/Button.kt
+++ app/src/main/java/com/sem5/codemy/features/profile/presentation/components/Button.kt
@@ -7,21 +7,17 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import com.sem5.codemy.features.auth.data.SignUpData
-import com.sem5.codemy.features.auth.presentation.viewmodel.AuthState
-import com.sem5.codemy.features.auth.presentation.viewmodel.AuthView
 import com.sem5.codemy.ui.theme.publicSansFontFamily
 
 @Composable
-fun LogoutButton(
+fun ProfileButton(
+    label: () -> String,
     onClick: () -> Unit
 ){
     Button(
@@ -38,18 +34,11 @@ fun LogoutButton(
         },
     ) {
         Text(
-            text = "Logout",
+            text = label(),
             fontFamily = publicSansFontFamily,
             fontWeight = FontWeight.Medium,
             fontSize = 12.sp,
             color = Color(0xFFF0F3FA)
         )
     }
-}
-
-
-@Preview
-@Composable
-fun LogoutButtonPreview(){
-    LogoutButton(onClick = {})
 }
--- app/src/main/java/com/sem5/codemy/features/profile/presentation/ui/Profile.kt
+++ app/src/main/java/com/sem5/codemy/features/profile/presentation/ui/Profile.kt
@@ -1,73 +1,46 @@
 package com.sem5.codemy.features.profile.presentation.ui
 
-import android.net.Uri
 import android.util.Log
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import coil.compose.AsyncImage
-import coil.compose.AsyncImagePainter.State.Empty.painter
-import coil.compose.rememberImagePainter
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
-import com.sem5.codemy.features.auth.data.User
 import com.sem5.codemy.ui.theme.components.BottomBar
 import com.sem5.codemy.ui.theme.components.TopBar
 import com.sem5.codemy.features.auth.presentation.viewmodel.AuthState
 import com.sem5.codemy.features.auth.presentation.viewmodel.AuthView
-import com.sem5.codemy.features.profile.data.remote.getUserProfile
-import com.sem5.codemy.features.profile.presentation.components.LogoutButton
+import com.sem5.codemy.features.profile.presentation.components.ProfileButton
 import com.sem5.codemy.features.profile.presentation.components.ProfileCard
-import com.sem5.codemy.features.profile.presentation.viewmodel.ProfileViewModel
 import com.sem5.codemy.ui.theme.DarkBlue
-import com.sem5.codemy.ui.theme.publicSansFontFamily
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
 
@@ -172,8 +145,14 @@ fun Profile(
                 userName = {userName.value ?: ""},
                 userEmail = {userEmail.value ?: ""}
             )
-
-            LogoutButton { authViewModel.signOut() }
+            ProfileButton(
+                label = {"Edit Profile"},
+                onClick = { navController.navigate("changeprofile") }
+            )
+            ProfileButton(
+                label = {"Logout"},
+                onClick = { authViewModel.signOut() }
+            )
         }
     }
 }
